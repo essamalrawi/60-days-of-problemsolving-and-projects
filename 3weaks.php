fcbfc7c2d6b5f1295b08a02cb88c67ed68d4dae1
Essam Al-Rawi <77260050+essamalrawi@users.noreply.github.com>
2023-02-15 16:08:05 +0200

    Add files via upload

---
+++ 3weaks.php
@@ -0,0 +1,117 @@
+<?php
+
+
+/*
+    echo 15.2 + 14.7 + (10.5 + 10.5); // 50
+    echo "\n";
+    echo gettype((int)15.2 + (int)14.7 + (int)(10.5 + 10.5)); // Integer 
+    echo "\n";
+
+
+   echo gettype(100);
+   echo "\n";
+   print gettype(100);
+   echo "\n";
+   Var_dump(gettype(100));
+// Method One
+// Method Two
+// Method Three => Optional 
+    
+    echo nl2br('We
+    Love
+    Elzero
+    Web
+    School
+
+    ')
+
+    echo <<<'Flop'
+    Hello "'Elzero'"'
+    We Love $Programming$
+    Languages Specially "PHP"
+    Flop;
+
+    $something = "Programming";
+
+    echo <<<"code"
+    Hello \PHP\
+    We Love $something
+    code;
+
+    // [1] Fix The Error
+    // [2] Remove 2 Characters To Get The Output
+
+
+    echo '<pre>';
+    print_r(['Front End' => ['HTML', 'CSS', 'JS' =>
+        ['Vuejs' => [2 =>'v2', 3=>'v3']], 0 
+        => 'Reacjs', 1 => 'Svelte'],
+         'Backend' => [0=>'PHP', 1=>'MySQL', 2 => 'Security'],
+         'Git',
+         'Github',
+         'Testing' => ['Unit Testing', 'End To End', 'Integration']
+    ]);
+    echo '</pre>';
+    */
+
+?>
+<!--
+<?php $username = 'Elzero Courses' ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Elzero Courses">
+    <title>Welcome To <?php echo $username?></title>
+  </head>
+  <body>
+    <h1><?php echo $username?></h1>
+    <p>Here In <?php echo $username?> We Provide Front-End And Back-End Courses</p>
+    <hr>
+    <div><?php echo $username?> Is The What You Need.</div>
+    <footer>All Right Reserved To <?php echo $username?></footer>
+  </body>
+</html>
+-->
+<?php
+/*
+    $name = "elzero";
+    $$name = "Web";
+    $b = &$$name;
+    echo $$name; // Web
+    echo "<br>"; 
+    echo "{$$name}"; // Web
+    echo "<br>"; 
+    echo $b; // Web
+    echo "<br>"; 
+    echo "web";
+    echo "<br>"; 
+    echo "$elzero";
+    */
+?>
+<?php
+/*
+echo $_SERVER['DOCUMENT_ROOT'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER["WINDIR"];
+echo "<br>";
+echo $_SERVER["OPENSSL_CONF"];
+
+// All Keys and Values of $_SERVER
+echo "<pre>";
+  echo print_r($_SERVER) ;
+echo "</pre>";
+*/?>
+<?php
+/*
+echo __LINE__;
+echo "<br>";
+echo __FILE__;
+echo "<br>";
+echo __DIR__;
+*/
+?>
